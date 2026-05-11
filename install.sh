d4a177cc2b2ab64ecce9ae950614295cf2a7cfed
rusher-code-330 <rusher.musique@gmail.com>
2026-05-11 20:11:26 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/data/data/com.termux/files/usr/bin/sh
+
+echo "Installing PGR Tools..."
+
+pkg install python -y
+
+pip install yt-dlp colorama pyfiglet
+
+chmod 755 pgr
+mv pgr $PREFIX/bin/
+chmod 755 $PREFIX/bin/pgr
+
+echo "Installed! Type: pgr"
