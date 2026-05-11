d99730f45aa48294d2a525d762677eccfa324e16
rusher-code-330 <rusher.musique@gmail.com>
2026-05-11 22:30:38 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -2,7 +2,7 @@
 
 echo Installing PGR Tools...
 
-:: Install Python packages
+
 pip install yt-dlp colorama pyfiglet
 
 echo.
@@ -12,11 +12,11 @@ echo.
 
 echo Setting up PGR command...
 
-:: Create pgr launcher
+
 echo @echo off > pgr.bat
 echo python %%USERPROFILE%%\pgr-tools\pgr.py %%* >> pgr.bat
 
-:: Move to PATH (Windows system folder)
+
 move pgr.bat C:\Windows\System32\
 
 echo.
