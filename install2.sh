a12fd6feccb464e941a6e94b7bb26add8dd793fc
rusher-code-330 <rusher.musique@gmail.com>
2026-05-12 11:43:09 +0200

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -5,7 +5,7 @@ echo "Installing PGR Tools..."
 
 sudo apt update
 
-
+sudo apt install nodejs -y
 sudo apt install ffmpeg -y
 sudo apt install python3 -y
 sudo apt install python3-pip -y
@@ -18,4 +18,4 @@ chmod 755 pgr
 sudo mv pgr /usr/local/bin/
 chmod 755 /usr/local/bin/pgr
 
-echo "Installed! Type: pgr"
+echo "Installed! Type: pgr2"
