6add2c4609e60d820a43dfb525923cf800452539
rusher-code-330 <rusher.musique@gmail.com>
2026-05-12 11:45:35 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -2,8 +2,10 @@
 
 echo Installing PGR Tools...
 
-
 pip install yt-dlp colorama pyfiglet
+
+echo Installing Node.js...
+winget install OpenJS.NodeJS
 
 echo.
 echo NOTE: ffmpeg must be installed manually on Windows
@@ -12,10 +14,8 @@ echo.
 
 echo Setting up PGR command...
 
-
 echo @echo off > pgr.bat
 echo python %%USERPROFILE%%\pgr-tools\pgr.py %%* >> pgr.bat
-
 
 move pgr.bat C:\Windows\System32\
 
